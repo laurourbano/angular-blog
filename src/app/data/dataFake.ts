b2940a84049053971528224d32bddf090e5d2f33
Lauro Otavio Urbano <lauro@outlook.com.br>
2024-01-11 00:25:19 -0300

    mudando os dados fakes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,32 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    id: '1',
+    title: 'Novo modelo da Renault',
+    description: 'Mais bajulado no salão do automóvel de SP',
+    photoCover:
+      'https://cdn.pixabay.com/photo/2016/09/15/12/12/renault-clio-1671405_1280.jpg',
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
-]
+    id: '2',
+    title: 'Novo modelo híbrido da Toyota',
+    description: 'Sempre em alta no mercado de automóveis híbridos',
+    photoCover:
+      'https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_1280.png',
+  },
+  {
+    id: '3',
+    title: 'Novo modelo da Fiat 2024',
+    description:
+      'Novo modelo da Fiat 2024 é lançado no salão do automóvel de SP',
+    photoCover:
+      'https://cdn.pixabay.com/photo/2016/12/07/21/50/car-1890494_1280.jpg',
+  },
+  {
+    id: '4',
+    title: 'Lançamento da nova Ferrari',
+    description:
+      'Lançamento da nova Ferrari é o mais esperado no salão do automóvel de SP',
+    photoCover:
+      'https://cdn.pixabay.com/photo/2023/05/03/16/11/alfa-romeo-7968027_1280.jpg',
+  },
+];
